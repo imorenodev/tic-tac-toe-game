0fd5a093c8d8c120c80fb09ac12ac278bfc28ca0
imoreno <imoreno@imorenos-MBP.fios-router.home>
2016-02-02 15:02:15 -0500

    modular design pattern attempt

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
-$(function() {
-  
-  /*hover cursor over unselected square shows player marker.
-  **Clicking unselected square applies player marker to square
-  **and unbinds all events from chosen square element. */
-  $(".square").click(function() {
-    $(this).html("<span>X</span>").unbind();
-  }).mouseenter(function() {
-    $(this).html("<span>X</span>");
-  }).mouseleave(function() {
-    $(this).html("");
-  });
-});
+var Board = (function() {
+  var $squares,
+      picked;
+
+  var init = function() {
+    $squares = $(".square");
+    picked = [];
+
+    $squares.on("click", function(e) {
+      $(e.target).html("X");
+      var squareNum = e.target.classList[1];
+
+      pickNum(squareNum);
+    });
+  }
+
+  function pickNum(squareNum) {
+    if (picked.indexOf(squareNum) === -1) {
+      picked.push(squareNum);
+      console.log('Added ' + squareNum + ' to picked');
+    }
+  }
+
+  return {
+    init: init
+  };
+})();
+
+$(document).ready(Board.init());
