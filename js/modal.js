88c0d06477e2093c33bec1d07c939f210247b9f2
imoreno <imoreno@imorenos-MBP.home>
2016-02-05 17:37:57 -0500

    added comments to modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,21 +1,28 @@
 var Modal = (function() {
+	// save modal element in variable
   var modal = document.querySelector(".modal");
-  var chosenMarker = document.querySelector(".modal");
 
+  // public open function
   function open() {
+  	/* add '.open' to modal class name and increase opacity to "1",
+  	   making modal window visible */
     modal.className = modal.className + " open";
-
-    chosenMarker.addEventListener("click", function(e) {
-    	
-    	if (e.target !== e.currentTarget && e.target.tagName !== "P") {
-    		modal.className = "modal";
-    		console.log(e.target.tagName);
-    	}
-      e.stopPropagation;
-    }, false);
+    
+    // add event listener to modal element
+    modal.addEventListener("click", chooseMarker, false);
   }
-
+  function chooseMarker(e) {
+	  /* only fire event if child elements (the markers) are clicked,
+  	   exluding the <p> element */
+  	if (e.target !== e.currentTarget && e.target.tagName !== "P") {
+  		close();
+  	}
+  	/* stop event bubbling past parent div / modal element the 
+  	   event listener is watching */
+    e.stopPropagation;
+  }
   function close() {
+  	// change modal element class name back to "modal", opacity "0"
   	modal.className = "modal";
   }
 
