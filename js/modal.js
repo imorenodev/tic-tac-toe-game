f02928ac7091f71562f11df2f8d19f7f644b0e19
imoreno <imoreno@imorenos-MBP.home>
2016-02-05 15:14:19 -0500

    removed ajax from the modal module

--- js/modal.js
+++ js/modal.js
@@ -2,10 +2,7 @@ var Modal = (function() {
   var $modal = $(".modal");
 
   function open() {
-  	$.ajax("modal.html", {dataType: "text"})
-  	  .then(function(contents) {
-  	  	$modal.html(contents).show();
-  	  });
+  	alert("Modal Open");
   }
 
 	return {
