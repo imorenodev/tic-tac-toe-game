7ce2df98c1eb58bd2a1ffe3f2aa397936da93504
imoreno <imoreno@imorenos-MBP.home>
2016-02-05 17:14:56 -0500

    added even listener for chosen marker on modal

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,22 @@
 var Modal = (function() {
   var modal = document.querySelector(".modal");
+  var chosenMarker = document.querySelector(".modal");
 
   function open() {
-
     modal.className = modal.className + " open";
-    console.log(modal.className);
+
+    chosenMarker.addEventListener("click", function(e) {
+    	
+    	if (e.target !== e.currentTarget && e.target.tagName !== "P") {
+    		modal.className = "modal";
+    		console.log(e.target.tagName);
+    	}
+      e.stopPropagation;
+    }, false);
+  }
+
+  function close() {
+  	modal.className = "modal";
   }
 
 	return {
